d8b958302a03c1789f16863e06451fb4dbac208d
javiercoulon@gmail.com <javiercoulon@gmail.com@fd8c2807-088c-fc8e-e176-6b3b2298f74b>
2014-10-01 04:51:19 +0000

    Una nueva actualizacion del proyecto, se termina la idea inicial de la estructura general del proyecto, se actualizan unos estilos. Se agregan uan fuentes para poder tenerlas en el navegador.

--- trunk/SIMUNI/web/recursos/scripts/js_paginas/script_main.js
+++ trunk/SIMUNI/web/recursos/scripts/js_paginas/script_main.js
@@ -7,9 +7,8 @@ window.onload = function() {
     userboxcomponent1 = chesdev_components_userbox_handler;
 
     userboxcomponent1.setUserText("Configuraciones Generales")
-    userboxcomponent1.addMenuItem("www.google.com", "Google");
-    userboxcomponent1.addMenuItem("www.google.com", "Google");
-    userboxcomponent1.addMenuItem("www.google.com", "Ir a configuraciones");
+    userboxcomponent1.addMenuItem("http://www.google.com", "Editar Cuenta");
+    userboxcomponent1.addMenuItem("www.google.com", "Hacer una consulta");
     userboxcomponent1.addMenuItem("www.google.com", "Cerrar Sesion");
     var configuraciones_usuario = document.getElementById("sm_div_usersettings");
     configuraciones_usuario.addEventListener('click', userboxcomponent1.chesdev_components_events_handler);
