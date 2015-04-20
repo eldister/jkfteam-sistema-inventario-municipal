7aa2a3499c6bc052ad363a1325b9fc1b29162a9a
javiercoulon <javiercoulon@gmail.com>
2015-04-20 03:55:11 +0000

    Se hace la documentación respectiva de los archivos manejadordatosactivos archivo recursos conexion mysql daemon 1 ireporteador utilidades servlet

--- simuniv2/src/java/simuni/entidades/daemons/Daemon1.java
+++ simuniv2/src/java/simuni/entidades/daemons/Daemon1.java
@@ -1,47 +1,71 @@
 package simuni.entidades.daemons;
 
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import simuni.clases.ln.ManejadorTareasRespaldo;
-import simuni.entidades.archivos.ManejadorRespaldos;
+import simuni.enums.Recursos;
 
+/**
+ * Esta clase se encarga de la gestión de tareas de manera background o más
+ * conocidos como Daemon, de allí su nombre. A partir de esta clase se deben
+ * hacer tareas en background como respaldos automáticos, reportes automáticos.
+ *
+ * @author FchescO
+ * @since 1.0
+ * @version 1.0
+ */
 public class Daemon1 implements ServletContextListener {
 
+    /**
+     * El objeto Timer para poder hacer las tareas cada cierto tiempo, que por
+     * defecto sera cada 20 segundos.
+     *
+     * @since 1.0
+     */
     private Timer timer;
 
+    /**
+     * Evento cuando se cierra la aplicación web.
+     *
+     * @param sce un objeto del tipo ServletContextEvent
+     * @since 1.0
+     */
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-       try{
-      
+        try {
+
             timer.cancel();
             timer.purge();
-       
-       }catch(Exception ex){
-           ex.printStackTrace();
-       }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
+    /**
+     * Evento cuando se levanta la aplicación web.
+     *
+     * @param sce un objeto del tipo ServletContextEvent
+     * @since 1.0
+     */
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         timer = new Timer();
-        TimerTask myTask = new TimerTask() {
+        TimerTask tarea_respaldo_automatico = new TimerTask() {
             @Override
             public void run() {
-                System.out.println("I'm doing awesome stuff right now."+new Date(System.currentTimeMillis()));
-                    ManejadorTareasRespaldo mtareasresp=new ManejadorTareasRespaldo();
-                    if(mtareasresp.hacerRespaldoBaseDatos(true)){
-                        System.out.println("Proceso supuestamente correcto");
-                    }
+                ManejadorTareasRespaldo mtareasresp = new ManejadorTareasRespaldo();
+                if (mtareasresp.hacerRespaldoBaseDatos(true)) {
+                    System.out.println("Proceso  correcto");
+                }
             }
         };
 
-        
-        long delay = 5000;
-        long period = 60 * 1000; // 10 seconds;
-        timer.scheduleAtFixedRate(myTask, delay, period);
+
+        timer.scheduleAtFixedRate(tarea_respaldo_automatico, Recursos.delay_respaldo_automatico,
+                Recursos.intervalo_respaldo_automatico);
     }
 
 }
